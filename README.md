0aadafab5a03da2cbc2850cfa8870f47d901250b
“yangxu” <404189928@qq.com>
2023-05-11 18:33:26 +0800

    feat: 🎸 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # changba-dialog-plugin
-to open dialog with instruction method in vue2
+
+open dialog with instruction method in vue2
+
+## install
+
+```bash
+npm i @changba/dialog-plugin
+```
+
+## usage
+
+how to import
+
+```js
+import Vue from "vue";
+import ChangbaDialogPlugin from "@changba/dialog-plugin";
+Vue.use(ChangbaDialogPlugin);
+```
+
+how to use
+
+```vue
+<script>
+export default {
+  name: "Demo",
+  methods: {
+    handleClick() {
+      // you have defined a component named by `Modal`
+      this.$openDialog("Modal");
+    },
+    handleClose() {
+      // close dialog
+      this.$closeDialog();
+    },
+  },
+};
+</script>
+```
---
+++ index.js
@@ -0,0 +1,70 @@
+const dialogInstance = [];
+
+export default {
+  install(Vue) {
+    function _close(destroyInstance) {
+      if (!destroyInstance) {
+        console.log("destroyInstance not exist");
+        return;
+      }
+      destroyInstance.$destroy();
+      document.body.removeChild(destroyInstance.$el);
+      dialogInstance = dialogInstance.filter((v) => v !== destroyInstance);
+    }
+
+    /**
+     * 打开一个弹窗
+     * @param dialogType
+     * @param params
+     * @return { any }
+     */
+    function openDialog(dialogType, params = { singleInstance: true }, events = {}) {
+      const outlet = document.createElement("div");
+      outlet.classList.add("dialog");
+      document.body.appendChild(outlet);
+      const { singleInstance, ...rest } = params;
+      const thisInstance = new Vue({
+        render: (h) => {
+          return h(dialogType, {
+            props: rest,
+            attrs: rest,
+            on: {
+              ...events,
+              close: _close,
+            },
+          });
+        },
+      }).$mount(outlet);
+      // 替换第一个instance，如果有
+      if (singleInstance) {
+        const firstInstance = dialogInstance.shift();
+        firstInstance && _close(firstInstance);
+        dialogInstance.unshift(thisInstance);
+      } else {
+        dialogInstance.push(thisInstance);
+      }
+      return { close: _close.bind(this, thisInstance), instance: thisInstance };
+    }
+
+    /**
+     * 关闭弹窗
+     * @returns
+     */
+    function closeDialog(destroyInstance) {
+      if (dialogInstance.length === 0) {
+        console.log("当前弹窗已经关闭，请先打开再关闭!");
+        return;
+      }
+      if (destroyInstance) {
+        _close(destroyInstance);
+      } else {
+        dialogInstance.forEach((ins) => {
+          _close(ins);
+        });
+      }
+    }
+
+    Vue.prototype.$closeDialog = closeDialog;
+    Vue.prototype.$openDialog = openDialog;
+  },
+};
