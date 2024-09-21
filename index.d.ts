ded32885419456126798d09aeb1ab212c0c19019
yangxu <yangxu01@changba.com>
2024-09-21 15:03:56 +0800

    feat: 🎸 merge

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 import { VueConstructor } from "vue";
 import { Store } from "vuex";
+import type { IVueI18n } from 'vue-i18n';
 interface PluginOptions {
     store?: Store<unknown>;
+    i18n?: IVueI18n;
 }
 declare const _default: {
     install(Vue: VueConstructor, options?: PluginOptions): void;
